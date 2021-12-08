8cc83a6a910c4f0630317244af976bb986eb8f1b
unswervingly <894184919@qq.com>
2021-12-08 11:39:04 +0800

    Square组件的状态提升

--- src/page/board/index.js
+++ src/page/board/index.js
@@ -6,8 +6,18 @@ import Square from '../square/index'
 
 export default class Board extends PureComponent {
 
+    // 状态提升
+    // 把Square的状态放到父组件保存
+    constructor(props) {
+        super(props);
+        this.state = {
+            // 将 Board 组件的初始状态设置为长度为 9 的空值数组
+            squares: Array(9).fill(null),
+        };
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return <Square key={i} value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
     }
 
 
@@ -17,16 +27,20 @@ export default class Board extends PureComponent {
         return (
             <div className="board">
                 <div className="status">{status}</div>
-                {this.renderSquare(1)}
-                {this.renderSquare(1)}
-                {this.renderSquare(1)}
-                {this.renderSquare(1)}
-                {this.renderSquare(1)}
-                {this.renderSquare(1)}
-                {this.renderSquare(1)}
-                {this.renderSquare(1)}
-                {this.renderSquare(1)}
+                {this.state.squares.map((item, index) => {
+                    return this.renderSquare(index)
+                })}
             </div >
         );
+    }
+
+    handleClick(i) {
+        // 创建了数组的一个副本
+        const newSquares = this.state.squares.slice()
+        // 根据传过来的 i,改变newSquares数组的值
+        newSquares[i] = "X"
+        this.setState({
+            squares: newSquares,
+        })
     }
 }
--- src/page/square/index.js
+++ src/page/square/index.js
@@ -1,21 +1,36 @@
-import React, { PureComponent } from "react";
+import React from "react";
 
 import './index.css'
 
-export default class Square extends PureComponent {
-    // 在Square组件中添加状态用来记录按钮被点击过
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null
-        };
-    }
+// export default class Square extends PureComponent {
+//     // 删除子组件Square的自有状态
+//     // constructor(props) {
+//     //     super(props);
+//     // 在Square组件中添加状态用来记录按钮被点击过
+//     //     this.state = {
+//     //         value: null
+//     //     };
+//     // }
 
-    render() {
-        return (
-            <button className="square" onClick={() => this.setState({ value: 'X' })}>
-                {this.state.value}
-            </button>
-        );
-    }
+//     render() {
+//         return (
+//             <button className="square" onClick={() => this.props.onClick()}>
+//                 {this.props.value}
+//             </button>
+//         );
+//     }
+// }
+
+
+// 优化子组件Square为函数组件，无状态使用函数组件更效率
+function Square(props) {
+
+    return (
+        // 在子组件Square修改按钮显示为父组件传来的参数，监听的点击方法也为父组件传来的方法
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
+
+export default Square
